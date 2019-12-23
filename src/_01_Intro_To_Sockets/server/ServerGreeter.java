a835fd17c9e0816374a732ee11d7afaab3612aef
League Student <league@iMac-27.attlocal.net>
2019-12-22 16:02:17 -0800

    ServerGreeter, ChatApp

--- src/_01_Intro_To_Sockets/server/ServerGreeter.java
+++ src/_01_Intro_To_Sockets/server/ServerGreeter.java
@@ -1,6 +1,9 @@
 package _01_Intro_To_Sockets.server;
 
 import java.net.*;
+
+import javax.swing.JOptionPane;
+
 import java.io.*;
 
 public class ServerGreeter extends Thread {
@@ -23,25 +26,29 @@ public class ServerGreeter extends Thread {
 			//   Put steps 8 - 15 in the try block.
 			try {
 				//8. Let the user know that the server is waiting for a client to connect.
-				
+				JOptionPane.showMessageDialog(null, "the server is waiting for a client to connect");
 				//9. Create an object of the Socket class and initialize it to serverSocket.accept();
 				//   Change serverSocket to match the ServerSocket member variable you created in step 1.
 				//   The program will wait her until either a client connects or the timeout expires.
-
+				Socket s = ss.accept();
 				//10. Let the user know that the client has connected.
-				
+				JOptionPane.showMessageDialog(null, "the client has connected");
 				//11. Create a DataInputStream object. When initializing it, use the Socket object you created in step 9 to call the getInputStream() method.
-
+				DataInputStream dis = new DataInputStream(s.getInputStream());
 				//12. Print the message from the DataInputStream object using the readUTF() method
-				
+				System.out.println(dis.readUTF());
 				//13. Create a DataOutputStream object. When initializing it, use the Server object you created in step 9 to call the getOutputStream() method.
-				
+				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 				//14. Use the DataOutputStream object to send a message to the client using the writeUTF(String message) method.
-				
+				dos.writeUTF("You've got a message!");
 				//15. Close the client server
 					
+			}catch(SocketTimeoutException e) {
+				JOptionPane.showMessageDialog(null, "experienced SocketTimeoutException");
+				bool = false;
 			}catch(IOException e) {
-				e.printStackTrace();
+				JOptionPane.showMessageDialog(null, "experienced IOException");
+				bool = false;
 			}
 			//6. If the program catches a SockeTimeoutException, let the user know about it and set loop's boolean variable to false.
 
@@ -52,6 +59,14 @@ public class ServerGreeter extends Thread {
 
 	public static void main(String[] args) {
 		//16. In a new thread, create an object of the ServerGreeter class and start the thread. Don't forget the try-catch.
-		
+		Thread th1 = new Thread(()->{
+			try{
+			    ServerGreeter sg = new ServerGreeter();
+			    sg.run();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		});
+		th1.run();
 	}
 }
